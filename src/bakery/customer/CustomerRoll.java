6e519a7286eb0419fb0505d65d7cb656dc4adcf3
djosborne <djosborne10@gmail.com>
2014-06-18 21:02:35 -0400

    commented customerRoll

--- src/bakery/customer/CustomerRoll.java
+++ src/bakery/customer/CustomerRoll.java
@@ -6,8 +6,15 @@ package bakery.customer;
  */
 public abstract class CustomerRoll {
 
+    /** Last Used ID from customers in this roll */
     private Integer lastUsedID = 1;
 
+    /**
+     * Starting at the lastUsedID, this function returns the nex ID which is not
+     * being used by a customer
+     * 
+     * @return Next open ID for a new customer to initialize with
+     */
     private Integer getNextAvailableID() {
         while (isReturningCustomer(lastUsedID)) {
             lastUsedID++;
@@ -16,15 +23,58 @@ public abstract class CustomerRoll {
         return lastUsedID;
     }
 
+    /**
+     * Searches through the customer roll for a customer which matches the
+     * provided ID and returns it
+     * 
+     * @param customerID
+     *            ID of the customer to find
+     * @return The Customer which matches the provided ID
+     */
     public abstract Customer getCustomer(Integer customerID);
 
+    /**
+     * Given all other pieces of information of a customer except ID, returns a
+     * matching customer, or throws exception if none exists
+     * 
+     * @param lastName
+     *            Last Name of the customer
+     * @param address
+     *            Address of the customer
+     * @param city
+     *            City of the customer
+     * @param state
+     *            State of the customer
+     * @param zipCode
+     *            Integer represenation of the zip code of the customer
+     * @return The customer which matches this information
+     */
     public abstract Integer getCustomerID(String lastName, String address,
         String city, String state, Integer zipCode);
 
-    // public abstract double getDiscountPoints(Integer customerID);
-
+    /**
+     * Searches for all customers who match the provided last name. May be more
+     * than one
+     * 
+     * @param lname
+     *            Last name of the customer to search for
+     * @return All customers who match this last name
+     */
     public abstract CustomerRoll getCustomersByLastName(String lname);
 
+    /**
+     * Creates a new customer roll identical to the one it is dynamic ally
+     * called on, except the user whose ID is provided will have his points set
+     * to the provided values
+     * 
+     * @param customerID
+     *            ID of customer whose points will be modified
+     * @param newAvailableDiscount
+     *            New available Discount Points the customer will have set
+     * @param loyaltyAmt
+     *            New loyalty amount the customer will have set
+     * @return Identical CustomRoll with modified user points
+     */
     public abstract CustomerRoll setPoints(Integer customerID,
         double newAvailableDiscount, double loyaltyAmt);
 
@@ -68,8 +118,24 @@ public abstract class CustomerRoll {
     abstract public boolean isReturningCustomer(String lastName,
         String address, String city, String state, Integer zipCode);
 
+    /**
+     * Function to check if the customer exists in the customerRoll
+     * 
+     * @param ID
+     *            ID of the customer to search for
+     * @return True if they already exist, False otherwise
+     */
     abstract public boolean isReturningCustomer(Integer ID);
 
+    /**
+     * Adds new customer to the customer roll, provided they are not already enrolled
+     * @param lastName Last name of the new customer
+     * @param address Address of the new customer
+     * @param city City of the new customer 
+     * @param state State of the new customer
+     * @param zipCode Zip code of the new customer
+     * @return CustomerRoll with the customer added
+     */
     public CustomerRoll addNewCustomer(String lastName, String address,
         String city, String state, Integer zipCode) {
         if (isReturningCustomer(lastName, address, city, state, zipCode)) {
@@ -83,6 +149,11 @@ public abstract class CustomerRoll {
         }
     }
 
+    /**
+     * Removes the customer with the given ID from the customerRoll
+     * @param customerID ID of the customer to be removed
+     * @return New Customerroll without the specified customer in it
+     */
     public abstract CustomerRoll removeCustomer(Integer customerID);
 
     public CustomerRoll addNewCustomer(Integer ID, String lastName,
