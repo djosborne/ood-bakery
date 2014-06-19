e597ebae72b70710972cc2b6ff1766c11209c428
djosborne <djosborne10@gmail.com>
2014-06-18 20:54:22 -0400

    added comments to customer

--- src/bakery/customer/Customer.java
+++ src/bakery/customer/Customer.java
@@ -1,6 +1,5 @@
 package bakery.customer;
 
-
 /**
  * Class to store customer data
  * 
@@ -22,21 +21,32 @@ public class Customer {
 
     /** customer's state */
     private String state;
-    
+
     /** customer's zipCode */
     private Integer zipCode;
-    
+
     private double loyaltyPoints;
-    
+
     private double availableDiscountPoints;
 
     /**
-     * Construct a new customer with given parameters
+     * Construct a new customer with given parameters. This function overloads
+     * the ID Generation and uses the provided ID. Also sets all loyalty points
+     * to 0
      * 
      * @param customerID
      *            customer's ID
      * @param lastName
      *            customer's last name
+     * @param address
+     *            customer's address
+     * 
+     * @param city
+     *            Customer's city
+     * @param state
+     *            Customer's State
+     * @param zipCode
+     *            integer represenatation of the customer's zip code
      */
     public Customer(Integer customerID, String lastName, String address,
         String city, String state, int zipCode) {
@@ -46,60 +56,123 @@ public class Customer {
         this.city = city;
         this.state = state;
         this.zipCode = zipCode;
-        
+
         this.availableDiscountPoints = 0;
         this.loyaltyPoints = 0;
     }
-    
+
+    /**
+     * Construct a new customer with given parameters. This function overloads
+     * the ID Generation and uses the provided ID. to 0
+     * 
+     * @param customerID
+     *            customer's ID
+     * @param lastName
+     *            customer's last name
+     * @param address
+     *            customer's address
+     * 
+     * @param city
+     *            Customer's city
+     * @param state
+     *            Customer's State
+     * @param zipCode
+     *            integer represenatation of the customer's zip code
+     * 
+     * @param discountPts
+     *            Number of discout points the customer has
+     * 
+     * @param loyaltyPts
+     *            number of loyalty points the customer has
+     */
     public Customer(Integer customerID, String lastName, String address,
-        String city, String state, int zipCode, double discountPts, double loyaltyPts) {
+        String city, String state, int zipCode, double discountPts,
+        double loyaltyPts) {
         this.customerID = customerID;
         this.lastName = lastName;
         this.address = address;
         this.city = city;
         this.state = state;
         this.zipCode = zipCode;
-        
+
         this.availableDiscountPoints = discountPts;
         this.loyaltyPoints = loyaltyPts;
     }
-    
+
+    /**
+     * Getter method for the customer's ID
+     * @return Customer's ID
+     */
     public Integer getCustomerID() {
         return customerID;
     }
-    
+
+    /**
+     * Getter method for the customer's last name
+     * @return Customer's last name
+     */
     public String getLastName() {
         return lastName;
     }
-    
+
+    /**
+     * Getter method for the customer's address
+     * @return Customer's address
+     */
     public String getAddress() {
         return address;
     }
-    
+
+    /**
+     * Getter method for the customer's city
+     * @return Customer's city
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * Getter method for the customer's State
+     * @return Customer's state
+     */
     public String getState() {
         return state;
     }
-    
+
+    /**
+     * Getter method for the customer's zip code
+     * @return customer's zip code
+     */
     public Integer getZipCode() {
         return zipCode;
     }
-    
+
+    /**
+     * Getter method for the customer's discount points
+     * @return Customer's discount points
+     */
     public double getDiscountPoints() {
         return availableDiscountPoints;
     }
-    
+
+    /**
+     * Getter method for the customer's loyalty points
+     * @return Customer's loyalty points
+     */
     public double getLoyaltyPoints() {
         return loyaltyPoints;
     }
-    
+
+    /**
+     * Customer Factory Creator which returns the same customer with new point values
+     * @param discountPts Number of discount points the new customer should have
+     * @param loyaltyPts number of loyalty points the new customer should have
+     * @return New customer with same params as old, but with new point values
+     */
     public Customer setPoints(double discountPts, double loyaltyPts) {
-        return new Customer(getCustomerID(), getLastName(), getAddress(), getCity(), getState(), getZipCode(), discountPts, loyaltyPts);
+        return new Customer(getCustomerID(), getLastName(), getAddress(),
+            getCity(), getState(), getZipCode(), discountPts, loyaltyPts);
     }
-  
 
     /**
      * String representation of this customer
@@ -108,7 +181,9 @@ public class Customer {
      */
     public String toString() {
         String sZipCode = String.format("%05d", zipCode);
-        return "[" + customerID + "] \t[" + lastName + "]\t[" + address + "]\t[" + city + "]\t[" + sZipCode + "] [" + getDiscountPoints() + "] [" + getLoyaltyPoints() + "]";
+        return "[" + customerID + "] \t[" + lastName + "]\t[" + address
+            + "]\t[" + city + "]\t[" + sZipCode + "] [" + getDiscountPoints()
+            + "] [" + getLoyaltyPoints() + "]";
     }
 
     /**
